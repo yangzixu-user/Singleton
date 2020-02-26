69f8476cc15299849829575d5456c93553794188
yangzixu-user <2513591994@qq.com>
2020-02-27 00:17:00 +0800

    Singleton-v-1.1

--- test/YapHaoTest.java
+++ test/YapHaoTest.java
@@ -8,66 +8,9 @@ package com.aaa.test;
  */
 public class YapHaoTest {
 
+    /**
+     * 黄凯你看我是谁！！！！！！！！！！！！！！！！！！！！！！！！！！！
+     */
 
-    public static void main(String[] args) {
-        YaoHao yaoHao = new YaoHao();
-        /*if (null == yaoHao.getName()){
-            yaoHao.setName("yangzixu");
-        }
-        System.out.println(yaoHao.getName());
-        yaoHao.setName("yangzixu02");
-        System.out.println(yaoHao.getName());*/
-
-        new Thread(()->{
-            for (int i = 0; i < 20; i++) {
-                if (null == yaoHao.getName()){
-                    yaoHao.setName("yangzixu");
-                }else {
-                    yaoHao.setName("yangzixu"+i);
-                }
-                System.out.println(yaoHao.getName());
-            }
-        },"yang1").start();
-        new Thread(()->{
-            for (int i = 0; i < 20; i++) {
-                if (null == yaoHao.getName()){
-                    yaoHao.setName("yangzixu");
-                }else {
-                    yaoHao.setName("yangzixu"+i);
-                }
-                System.out.println(yaoHao.getName());
-            }
-        },"yang2").start();
-        new Thread(()->{
-            for (int i = 0; i < 20; i++) {
-                if (null == yaoHao.getName()){
-                    yaoHao.setName("yangzixu");
-                }else {
-                    yaoHao.setName("yangzixu"+i);
-                }
-                System.out.println(yaoHao.getName());
-            }
-        },"yang3").start();  new Thread(()->{
-            for (int i = 0; i < 20; i++) {
-                if (null == yaoHao.getName()){
-                    yaoHao.setName("yangzixu");
-                }else {
-                    yaoHao.setName("yangzixu"+i);
-                }
-                System.out.println(yaoHao.getName());
-            }
-        },"yang4").start();  new Thread(()->{
-            for (int i = 0; i < 20; i++) {
-                if (null == yaoHao.getName()){
-                    yaoHao.setName("yangzixu");
-                }else {
-                    yaoHao.setName("yangzixu"+i);
-                }
-                System.out.println(yaoHao.getName());
-            }
-        },"yang5").start();
-
-
-    }
 }
 
